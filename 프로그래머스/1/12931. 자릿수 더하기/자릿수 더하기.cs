9ecfe588b5e18baf037549d4b26242f9f9842723
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-01-14 19:07:13 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -3,6 +3,11 @@ using System.Linq;
 
 public class Solution {
     public int solution(int n) {
-        return n.ToString().Select(c => int.Parse(c.ToString())).Sum();
+        int sum = 0;
+        do{
+            sum += n % 10;
+            n /= 10;
+        }while(n != 0);
+        return sum;
     }
 }
