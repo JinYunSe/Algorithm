f4a91317d4b709019e07c67c29b42adf64bd966a
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-01-20 14:08:57 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 4.08 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
@@ -1,8 +1,7 @@
 using System.Linq;
 
 public class Solution {
-    public string solution(string s)
-    {
-        return string.Join("",s.Select((element)=> (int)element).OrderByDescending((element)=> element).Select((element)=> (char)element).ToArray());    
+    public string solution(string s) {
+        return string.Concat(s.OrderByDescending(element => element));
     }
 }
