f57b3314857640c0e4c36f3bca3fe0f92d3e0702
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-01-20 13:53:36 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.44 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -11,12 +11,13 @@ public class Solution {
             int count = 0;
             for(int j = 1; j <= (int)Math.Sqrt(i); j++)
             {
-                if(i % j == 0 && i / j == j) count++;
-                else if(i % j == 0) count += 2;
+                // if(i % j == 0 && i / j == j) count++;
+                // else if(i % j == 0) count += 2;
+                
+                count += i % j == 0 ? i / j == j ? 1 : 2 : 0;
             }
             
-            if(count % 2 == 0) sum += i;
-            else sum -= i;
+            sum += (count % 2 == 0) ? i : -1 * i;
         }
         
         return sum;
