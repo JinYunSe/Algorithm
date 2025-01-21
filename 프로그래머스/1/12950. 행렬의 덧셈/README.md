7cebae3285c922125aa41340d20a5474954f6ff6
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-01-21 14:55:47 +0900

    [level 1] Title: 행렬의 덧셈, Time: 1.98 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/README.md
+++ 프로그래머스/1/12950. 행렬의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 행렬의 덧셈 - 12950 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12950?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12950) 
 
 ### 성능 요약
 
-메모리: 52 MB, 시간: 8.88 ms
+메모리: 53.2 MB, 시간: 1.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 21일 14:14:51
+2025년 01월 21일 14:55:46
 
 ### 문제 설명
 
--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cs
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cs
@@ -1,13 +1,17 @@
-using System.Linq;
-
 public class Solution {
-    public int[,] solution(int[,] arr1, int[,] arr2) 
-    {
-        for(int i = 0; i < arr1.GetLength(0); i++)
-        {
-            for(int j = 0; j < arr1.GetLength(1); j++)
-                arr1[i,j] += arr2[i,j];
+    public int[][] solution(int[,] arr1, int[,] arr2) {
+        int rows = arr1.GetLength(0);
+        int cols = arr1.GetLength(1);
+        int[][] answer = new int[rows][];
+
+        for (int i = 0; i < rows; i++) {
+            answer[i] = new int[cols];
+
+            for (int j = 0; j < cols; j++) {
+                answer[i][j] = arr1[i, j] + arr2[i, j];
+            }
         }
-        return arr1;
+
+        return answer;
     }
 }
