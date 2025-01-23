6d5370d7620db418f9db75c1e3ba30383eac959d
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-01-23 15:43:57 +0900

    [level 1] Title: 직사각형 별찍기, Time: 20.36 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/README.md
+++ 프로그래머스/1/12969. 직사각형 별찍기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 직사각형 별찍기 - 12969 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969) 
 
 ### 성능 요약
 
-메모리: 16.7 MB, 시간: 135.37 ms
+메모리: 16.8 MB, 시간: 20.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 13:45:00
+2025년 01월 23일 15:43:55
 
 ### 문제 설명
 
--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cs
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cs
@@ -12,13 +12,9 @@ public class Example
         int a = Int32.Parse(s[0]);
         int b = Int32.Parse(s[1]);
 
-        for( int i=0; i<b; i++ )
-        {            
-            for( int j=0; j<a; j++ )
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine();
+        for(int i = 0; i < b; i++)
+        {
+            Console.WriteLine(new string('*',a));
         }
     }
 }
