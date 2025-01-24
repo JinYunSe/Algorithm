01b7c18512979e564c63f292298ba357a064e6c8
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-01-24 14:57:11 +0900

    [level 1] Title: 시저 암호, Time: 0.64 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cs
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cs
@@ -9,15 +9,12 @@ public class Solution {
         
         for(int i = 0; i < charArray.Length; i++)
         {
-            if(charArray[i] == ' ') continue;
+            if(charArray[i] == ' ') 
+                continue;
             else if(char.IsLower(charArray[i]))
-            {
-                charArray[i] = (int)charArray[i] + n > (int)'z' ? (char)(charArray[i] - 26 + n) : (char)(charArray[i] + n);
-            }
+                charArray[i] = charArray[i] + n > 'z' ? (char)(charArray[i] - 26 + n) : (char)(charArray[i] + n);
             else
-            {
-                charArray[i] = (int)charArray[i] + n > (int)'Z' ? (char)(charArray[i] - 26 + n) : (char)(charArray[i] + n);
-            }
+                charArray[i] = charArray[i] + n > 'Z' ? (char)(charArray[i] - 26 + n) : (char)(charArray[i] + n);
         }
         
         return new string(charArray);
