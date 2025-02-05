07004f03bada5168b9100b4b519d6c4deaa005ce
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-05 15:52:21 +0900

    [level 1] Title: 명예의 전당 (1), Time: 44.12 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public class Solution 
 {
     public int[] solution(int k, int[] score) 
     {
+        List<int> list = new List<int>();
         int[] answer = new int[score.Length];
         
         for(int i = 0; i < score.Length; i++)
         {
-           int[] temp = score.Skip(0).Take(i + 1).OrderByDescending(element => element).ToArray();
-            answer[i] = temp.Length < k ? temp.Min() : temp[k - 1];
+            list.Add(score[i]);
+            list.Sort();
+            list.Reverse();
+            answer[i] = list.Count < k ? list.Min() : list[k - 1];
         }
         return answer;
     }
