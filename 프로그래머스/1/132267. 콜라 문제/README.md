c48922949f0c209ecc368ebcbbca4e64648f212d
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-05 15:23:38 +0900

    [level 1] Title: 콜라 문제, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/README.md
+++ 프로그래머스/1/132267. 콜라 문제/README.md
@@ -1,6 +1,6 @@
 # [level 1] 콜라 문제 - 132267 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=csharp) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 15:23:18
+2025년 02월 05일 15:23:37
 
 ### 문제 설명
 
--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -4,12 +4,25 @@ public class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
         
-        while(n / a > 0)
+        while(int.Parse(n / a) > 0)
         {
-            answer += n / a * b;
-            n = n / a * b + n % a;
+            answer += int.Parse(n / a) * b;
+            n = int.Parse(n / a) * b + n % a;
         }
         
         return answer;
     }
 }
+/*
+
+function solution(a, b, n) {
+    let answer = 0;
+    while(parseInt(n / a) > 0)
+    {
+        answer += parseInt(n / a) * b
+        n = parseInt(n / a) * b + n % a
+    }
+    return answer
+}
+
+*/
