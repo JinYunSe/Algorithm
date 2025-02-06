9f3ba3ec9629b0d58daa1b0e2801840a7f733497
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-06 14:33:13 +0900

    [level 1] Title: 카드 뭉치, Time: 3.94 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public string solution(string[] cards1, string[] cards2, string[] goal) 
+    {
+        Queue<string> queue1 = new Queue<string>(cards1);
+        Queue<string> queue2 = new Queue<string>(cards2);
+        
+        foreach(var element in queue2)
+            Console.WriteLine(element);
+        
+        bool check = true;
+        for(int i = 0; i < goal.Length; i++)
+        {
+            if(queue1.Count > 0 && goal[i].Equals(queue1.Peek())) queue1.Dequeue();
+            else if(queue2.Count > 0 && goal[i].Equals(queue2.Peek())) queue2.Dequeue();
+            else
+            {
+                check = false;
+                break;
+            }
+        }
+        return check ? "Yes" : "No";
+    }
+}
