b6e2888ffaeba8045da93582469d7b5f41b27c47
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-07 15:59:03 +0900

    [level 1] Title: 덧칠하기, Time: 2.34 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
@@ -1,8 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 public class Solution {
-    public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        return answer;
+    public int solution(int n, int m, int[] section) 
+    {
+        Queue<int> queue = new Queue<int>(section);
+        
+        int count = 0;
+        
+        while(queue.Count != 0)
+        {
+            
+            int current = queue.Dequeue();
+            count++;
+            while(queue.Count > 0 &&queue.Peek() < current + m) queue.Dequeue();
+        }
+        
+        return count;
     }
 }
