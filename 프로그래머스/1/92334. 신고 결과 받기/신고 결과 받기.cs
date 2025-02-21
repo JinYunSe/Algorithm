c64ede6094e18ccc720be5eac4427d41186d1fe6
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-22 00:55:16 +0900

    [level 1] Title: 신고 결과 받기, Time: 469.33 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(string[] id_list, string[] report, int k) 
+    {
+        report = report.Distinct().ToArray();
+        List<int> list = new List<int>();
+        Dictionary<string, int> bans = new Dictionary<string,int>();
+        Dictionary<string, List<string>> reporter = new Dictionary<string, List<string>>();
+        
+        foreach(string id in id_list)
+        {
+            reporter[id] = new List<string>();
+        }
+        
+        for(int i = 0; i < report.Length; i++)
+        {
+            string[] names = report[i].Split(' ');
+            
+            if(!reporter[names[0]].Contains(names[1]))
+            {
+                reporter[names[0]].Add(names[1]);
+                
+                if(!bans.ContainsKey(names[1]))
+                    bans[names[1]] = 0;
+                
+                bans[names[1]]++;
+            }
+        }
+        
+        foreach (var entry in reporter)
+        {
+            int count = 0;
+            foreach (string reported in entry.Value)
+            {
+                if (bans[reported] >= k)
+                    count++;
+                
+            }
+            list.Add(count);
+        }
+
+        
+        return list.ToArray();
+    }
+}
