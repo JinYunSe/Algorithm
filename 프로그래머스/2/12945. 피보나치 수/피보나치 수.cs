f67af8fecf1e764399f1682d986719eac3db31d6
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-25 14:59:38 +0900

    [level 2] Title: 피보나치 수, Time: 0.99 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
@@ -4,16 +4,14 @@ using System.Collections.Generic;
 public class Solution {
     public int solution(int n) 
     {
-        List<int> list = new List<int>();
         
-        list.Add(0);
-        list.Add(1);
-        
+        int zero = 0, one = 1, sum = 1;
         for(int i = 2; i <= n; i++)
         {
-            list.Add((list[i - 1] + list[i - 2]) % 1234567);
+            sum = (one + zero) % 1234567;
+            zero = one;
+            one = sum;
         }
-        
-        return list[n];
+        return sum;
     }
 }
