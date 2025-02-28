fedba70635557fce55b3a5c4db042b00958a69d3
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-28 14:23:41 +0900

    [level 2] Title: N개의 최소공배수, Time: 5.19 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] arr) 
+    {
+        arr = arr.Where(element => element != 1).OrderBy(element => element).ToArray();
+        
+        for(int i = 0; i < arr.Length - 1; i++)
+        {
+            int big = arr[i + 1], small = arr[i];
+            while(arr[i] != 0)
+            {
+                int temp  = arr[i + 1] % arr[i];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+            
+            arr[i + 1] = big * small / arr[i + 1]; 
+        }
+        
+        return arr[arr.Length - 1];
+    }
+}
