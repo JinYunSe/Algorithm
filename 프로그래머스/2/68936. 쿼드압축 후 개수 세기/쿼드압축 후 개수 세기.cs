f7f0e26033d3f57139d38b64922221fff927d0de
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-04-22 14:21:58 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 27.54 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cs
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cs
@@ -1,43 +1,16 @@
 using System;
 
 public class Solution {
-    
-    private int[] answer = new int[] {0,0};
-    
-    public int[] solution(int[,] arr) 
-    {
-        QuadCompression(0,0,arr.GetLength(0), arr);
-        return answer;
-    }
-    
-    public void QuadCompression(int x, int y, int len, int[,] arr)
-    {
-        bool checkZero = true;
-        bool checkOne = true;
-        
-        for(int i = x; i < x + len; i++)
+    public int[] solution(int[,] arr) {
+        for(int i = 0; i < arr.GetLength(0); i++)
         {
-            for(int j = y; j < y + len; j++)
+            for(int j = 0; j < arr.GetLength(1); j++)
             {
-                if(arr[i, j] == 0) checkOne = false;
-                if(arr[i, j] == 1) checkZero = false;
+                Console.Write(arr[i,j]+" ");
             }
+            Console.WriteLine();
         }
         
-        if(checkZero) 
-        {
-            answer[0]++;
-            return;
-        }
-        if(checkOne) 
-        {
-            answer[1]++;
-            return;
-        }
-        
-        QuadCompression(x, y, len / 2, arr);
-        QuadCompression(x + len / 2, y, len / 2, arr);
-        QuadCompression(x, y + len / 2, len / 2, arr);
-        QuadCompression(x + len / 2, y + len / 2, len / 2, arr);
+        return new int[] {};
     }
 }
