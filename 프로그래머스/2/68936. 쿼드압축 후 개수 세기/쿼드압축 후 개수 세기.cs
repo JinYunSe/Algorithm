06fb895e96e22ad9850ef7f617f278bbae587017
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-04-22 14:27:46 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 28.75 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cs
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cs
@@ -1,16 +1,53 @@
 using System;
 
 public class Solution {
-    public int[] solution(int[,] arr) {
-        for(int i = 0; i < arr.GetLength(0); i++)
+    
+    // 0과 1 결과 담을 그릇
+    private int[] answer = new int[] {0,0};
+    
+    public int[] solution(int[,] arr) 
+    {
+        QuadCompression(0,0,arr.GetLength(0), arr);
+        return answer;
+    }
+    
+    // 필요한 매개변수
+    // 시작 x, y 위치
+    // 배열 탐색 범위
+    // 2차원 배열
+    public void QuadCompression(int x, int y, int len, int[,] arr)
+    {
+        bool checkZero = true;
+        bool checkOne = true;
+        
+        for(int i = x; i < x + len; i++)
         {
-            for(int j = 0; j < arr.GetLength(1); j++)
+            for(int j = y; j < y + len; j++)
             {
-                Console.Write(arr[i,j]+" ");
+                if(arr[i, j] == 0) checkOne = false;
+                if(arr[i, j] == 1) checkZero = false;
             }
-            Console.WriteLine();
         }
         
-        return new int[] {};
+        // 모든 값이 0 또는 1이면 개수 증가 및 재귀함수 종료
+        if(checkZero) 
+        {
+            answer[0]++;
+            return;
+        }
+        if(checkOne) 
+        {
+            answer[1]++;
+            return;
+        }
+        
+        // 원쪽 위 탐색
+        QuadCompression(x, y, len / 2, arr);
+        // 오른쪽 위 탐색
+        QuadCompression(x + len / 2, y, len / 2, arr);
+        // 윈쪽 아래 탐색
+        QuadCompression(x, y + len / 2, len / 2, arr);
+        // 오른쪽 아래 탐색
+        QuadCompression(x + len / 2, y + len / 2, len / 2, arr);
     }
 }
