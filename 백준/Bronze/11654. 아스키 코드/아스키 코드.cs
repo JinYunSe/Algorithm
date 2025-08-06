335143b6b1f1752e564bf31ecf35debea6f29b60
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-08-07 01:13:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 64 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,12 @@
+using System;
+namespace CodingTest
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            char alpabat = char.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToInt16(alpabat));
+        }
+    }
+}
