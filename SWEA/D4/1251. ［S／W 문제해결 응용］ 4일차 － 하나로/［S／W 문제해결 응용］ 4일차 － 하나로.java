7050d304d44d25d86f2d5535bdbbdc1708760738
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-09-05 19:28:40 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,246 ms, Memory: 88,480 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,93 +1,70 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class Solution {
 	
-	public static class UnionFind
-	{
-		private int[] parent;
-		private int[] rank;
-		
-		public UnionFind(int n)
-		{
-			this.parent = new int[n + 1];
-			this.rank = new int[n + 1];
-			
-			for(int i = 0; i < this.parent.length; i++)
-			{
-				this.parent[i] = i;
-				this.rank[i] = 0;
-			}
-		}
-		
-		public int find(int x)
-		{
-			if(this.parent[x] != x) this.parent[x] = find(this.parent[x]);
-			return this.parent[x];
-		}
-		
-		public void union(int x, int y)
-		{
-			int rootX = find(x), rootY = find(y);
-			
-			if(rootX == rootY) return;
-			
-			if(this.rank[rootX] > this.rank[rootY])
-			{
-				this.parent[rootY] = rootX;
-			}
-			else if(this.rank[rootX] > this.rank[rootY])
-			{
-				this.parent[rootX] = rootY;
-			}
-			else
-			{
-				this.parent[rootY] = rootX;
-				this.rank[rootX]++;
-			}
-		}
-		
-		public boolean isConnected(int x, int y)
-		{
-			return find(x) == find(y);
-		}
-	}
+	private static int[] rank;
+	private static int[] parent;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader rb = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int T = Integer.parseInt(rb.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
-		for(int test_case = 1; test_case <=T; test_case++)
+		for(int test_case = 1; test_case <= T; test_case++)
 		{
-			int input = Integer.parseInt(rb.readLine());
+			int size = Integer.parseInt(br.readLine());
+
+			parent = new int[size];
+			rank = new int[size];
 			
-			int[] x = new int[input];
+			for(int i = 0; i < parent.length; i++)
+			{
+				parent[i] = i;
+				rank[i] = 0;
+			}
+
+			int edgeCount = (size - 1) * size / 2;
 			
-			String[] tempX = rb.readLine().split(" ");
+			int[] x = new int[size];
 			
+			StringTokenizer stz = new StringTokenizer(br.readLine());
 			for(int i = 0; i < x.length; i++)
 			{
-				x[i] = Integer.parseInt(tempX[i]);
+				x[i] = Integer.parseInt(stz.nextToken());
 			}
 			
-			int[] y = new int[input];
+			int[] y = new int[size];
 			
-			String[] tempY = rb.readLine().split(" ");
-			
+			stz = new StringTokenizer(br.readLine());
 			for(int i = 0; i < y.length; i++)
 			{
-				y[i] = Integer.parseInt(tempY[i]);
+				y[i] = Integer.parseInt(stz.nextToken());
 			}
 			
-			double e = Double.parseDouble(rb.readLine());
+			long[][] table = new long[edgeCount][3];
 
-			int edgeCount = (input * (input - 1)) / 2;
-			long[][] edge = new long[edgeCount][3];
-			makeEdgeAndWeight(x, y, e, edge);
 			
-			Arrays.sort(edge, new Comparator<long[]>() {
+			int z = 0;
+			
+			for(int i = 0; i < size - 1; i++)
+			{
+				for(int j = i + 1; j < size; j++)
+				{
+					table[z][0] = i;
+					table[z][1] = j;
+					table[z][2] = (long) (Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2));
+					z++;
+				}
+			}
+			
+			double e = Double.parseDouble(br.readLine());
+			
+			long cost = 0;
+			
+			
+			Arrays.sort(table, new Comparator<long[]>() {
 
 				@Override
 				public int compare(long[] o1, long[] o2) {
@@ -96,38 +73,59 @@ public class Solution {
 				}
 			});
 			
-			long cost = kruskalSearch(edge, input);
+			for(int i = 0; i < table.length; i++)
+			{
+				int left = (int)table[i][0];
+				int right = (int)table[i][1];
+				
+				if(find(left) != find(right))
+				{
+					cost += table[i][2];
+					union(left, right);
+				}
+			}
 			
-			System.out.println("#"+test_case+" "+Math.round((double)cost * e));
+			System.out.println("#"+test_case+" "+Math.round((e * cost)));
 		}
 	}
 	
-	public static void makeEdgeAndWeight(int[] x, int[] y, double e, long[][] edge)
+    // 부모 찾는 메서드
+	public static int find(int x)
 	{
-		int z = 0;
-		for(int i = 0; i < x.length - 1; i++)
-		{
-			for(int j = i + 1; j < y.length; j++)
-			{
-				edge[z][0] = i;
-				edge[z][1] = j;
-				edge[z++][2] = (long) (Math.pow((x[i] - x[j]), 2) + Math.pow((y[i] - y[j]), 2));
-			}
-		}
+        // 조건문 : 나 자신 스스로가 부모(최상위 root 노드)인가?
+        // 아니면 부모의 부모를 계속 찾아감
+        // => 결국 (최상위 root 노드)를 찾게 됨
+		if(x != parent[x]) parent[x] = find(parent[x]);
+        //최상위 root 노드 반환
+		return (int) parent[x];
 	}
 	
-	public static long kruskalSearch(long[][] edge, int input)
+    // 서로 다른 집합을 하나의 집합으로 만드는 메서드
+	public static void union(int x, int y)
 	{
-		UnionFind union = new UnionFind(input);
-		long cost = 0;
-		for(int i = 0; i < edge.length; i++)
+        // x의 최상위 root 노드, y의 최상위 root 노드 가져오기
+		int rootX = find(x), rootY = find(y);
+		
+		if(rootX == rootY) return;
+		
+        // rank[rootX] > rank[rootY]로
+        // rank(높이)가 누가 높은지 비교
+		if(rank[rootX] > rank[rootY])
 		{
-			if(!union.isConnected((int)edge[i][0], (int)edge[i][1]))
-			{
-				union.union((int)edge[i][0], (int)edge[i][1]);
-				cost += edge[i][2];
-			}
+            // rootX가 높으므로 rootY의 부모를 rootX로 지정
+			parent[rootY] = rootX;
 		}
-		return cost;
+		else if(parent[rootY] > parent[rootX])
+		{
+            // rootY가 높으므로 rootX의 부모를 rootY로 지정
+			parent[rootX] = rootY;
+		}
+        // 높이가 같으므로 한 대상 잡고 rank를 증가시켜주고
+        // rank를 증가시키지 않은 대상의 부모를 rank를 증사키신 대상으로 만듬
+		else 
+		{
+			parent[rootY] = rootX;
+			rank[rootX]++;
+		}
 	}
 }
