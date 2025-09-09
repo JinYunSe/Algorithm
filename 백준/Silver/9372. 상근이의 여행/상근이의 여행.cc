9d9248b1744577fd8264918e6f236ea4f0b456f1
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-09-09 17:27:29 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -1,17 +1,59 @@
 #include <iostream>
-using namespace std;
+#include <algorithm>
 
-int main(){
-   int T;
-    cin >> T;
-    int n, m;
-    for(int tc = 1; tc <= T; tc++){
-        cin >> n >> m;
-        for(int i = 0; i < m; i++){
-            int a, b;
-            cin >> a >> b;
-        }
-        
-        cout << n-1 << "\n";
-    }
+using namespace std;
+int n, m;
+pair<int, int> edge[10001];
+int p[1001];
+
+int find(int x) {
+	if (p[x] < 0)
+		return x;
+
+	return p[x] = find(p[x]);
+}
+
+bool uni(int u, int v) {
+	u = find(u);
+	v = find(v);
+
+	if (u == v)
+		return false;
+
+	if (p[v] < p[u])
+		swap(p[u], p[v]);
+
+	if (p[u] == p[v])
+		p[u]--;
+
+	p[v] = u;
+	return true;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		cin >> n >> m;
+		int a, b;
+		for (int i = 0; i < m; i++) {
+			cin >> a >> b;
+			edge[i] = { a, b };
+		}
+
+		int cnt = 0;
+		fill(p, p + n + 1, -1);
+
+		for (int i = 0; i < m && cnt < n-1; i++) {
+			pair<int, int> curr = edge[i];
+			if (uni(curr.first, curr.second)) {
+				cnt++;
+			}
+		}
+
+		cout << cnt << "\n";
+	}
 }
