3466cbe038efacb32ee0c55abb095138e28f7868
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-09-09 17:25:59 +0900

    [Silver IV] Title: 상근이의 여행, Time: 72 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int T;
+    cin >> T;
+    int n, m;
+    for(int tc = 1; tc <= T; tc++){
+        cin >> n >> m;
+        for(int i = 0; i < m; i++){
+            int a, b;
+            cin >> a >> b;
+        }
+        
+        cout << n-1 << "\n";
+    }
+}
