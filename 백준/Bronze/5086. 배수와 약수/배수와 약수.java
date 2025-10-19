1a9925df58286516c41a5e45b6b1ba1d0d1d82ad
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-10-19 22:30:19 +0900

    [Bronze III] Title: 배수와 약수, Time: 96 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -7,28 +7,22 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		// while(true)를 통해 무한 반복문을 만들어준다.
+		// 향후, 입력으로 0 0이 입력되면 break로 무한 반복문 탈출 예정
 		while(true)
 		{
 			StringTokenizer stz = new StringTokenizer(br.readLine());
 			
+			// int형 변수 2개에 값을 저장
 			int num1 = Integer.parseInt(stz.nextToken());
 			int num2 = Integer.parseInt(stz.nextToken());
 			
+			// 0 0 이면 반복문 종료
 			if(num1 == 0 && num2 == 0) break;
 			
-			boolean check = true;
-			if(num1 >= num2) 
-			{
-				check = num1 % num2 == 0;
-				if(check) bw.write("multiple\n");
-			}
-			else 
-			{
-				check = num2 % num1 == 0;	
-				if(check) bw.write("factor\n");
-			}	
+			String result = num1 % num2 == 0 ? "multiple\n" : num2 % num1 == 0 ? "factor\n" : "neither\n";
 			
-			if(!check) bw.write("neither\n");
+			bw.write(result);
 		}
 
 		bw.flush();
