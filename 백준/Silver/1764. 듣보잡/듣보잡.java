a456158bac13802cf3d52b85c925390376ac7308
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-10-20 16:24:08 +0900

    [Silver IV] Title: 듣보잡, Time: 280 ms, Memory: 26544 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -2,34 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer str = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(str.nextToken());
-		int m = Integer.parseInt(str.nextToken());
-		
-		HashMap<String, Integer> dict = new HashMap<String, Integer>();
-		
-		List<String> list = new ArrayList<String>();
-		for(int i = 0; i < n + m; i++)
-		{
-			String temp = br.readLine();
-			if(!dict.containsKey(temp)) dict.put(temp, 1);
-			else list.add(temp);
-		}
-		
-		Collections.sort(list);
-		
-		bw.write(list.size()+"");
-		bw.newLine();
-		for(String temp : list)
-		{
-			bw.write(temp);
-			bw.newLine();
-		}
-		bw.flush();
-	}
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	StringTokenizer stz = new StringTokenizer(br.readLine());
+    	
+    	int n = Integer.parseInt(stz.nextToken());
+    	int m = Integer.parseInt(stz.nextToken());
+    	
+    	HashSet<String> map = new HashSet<String>();
+    	
+    	for(int i = 0; i < n; i++) map.add(br.readLine());
+    	
+    	List<String> result = new ArrayList<String>();
+    	for(int i = 0; i < m; i++) 
+    	{
+    		String temp = br.readLine();
+    		if(map.contains(temp)) result.add(temp);
+    	}
+    	    
+    	Collections.sort(result);
+    	
+    	bw.write(result.size()+"\n");
+    	
+    	for(String name : result)
+    	{
+    		bw.write(name+"\n");
+    	}
+    	
+    	bw.flush();
+    }
 }
